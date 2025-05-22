deeb6575ddb40e5a46db8fb8d855648ab865524e
harshada <harshadachaudhari0122@gmail.com>
2025-05-22 14:41:37 +0530

    Code Upload

---
+++ CRUD_Assignment_ZesTec/BAL/CustomerBAL.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Razor.Tokenizer;
+
+namespace CRUD_Assignment_ZesTec.BAL
+{
+	public class CustomerBAL
+	{
+		string connectionString = "Server=DESKTOP-NU466DS;Database=CRUD_ZesTec;Integrated Security=True";
+	
+		public void SaveAddCustomer(NameValueCollection form)
+		{
+			using (SqlConnection con = new SqlConnection(connectionString))
+			{
+				string query = @"INSERT INTO CustomerMaster (customer_name, mobile_no,gender, email_id, address, pincode) VALUES ('" + form["CustomerName"] + "', '"+ form["Mobileno"] + "','"+ form["Gender"] + "', '"+ form["EmailID"] + "', '"+ form["Address"] + "', '"+ form["Pincode"] + "')";
+
+				SqlCommand cmd = new SqlCommand(query, con);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				con.Close();
+			}
+		}
+
+		
+		public DataSet GetEditCustomerData(int id)
+		{
+
+			DataSet ds = new DataSet();
+
+			try
+			{
+				using (SqlConnection conn = new SqlConnection(connectionString))
+				{
+					string query = "SELECT * FROM CustomerMaster where id="+id+"";
+					using (SqlDataAdapter adapter = new SqlDataAdapter(query, conn))
+					{
+						adapter.Fill(ds);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error fetching customer data: " + ex.Message);
+			}
+			return ds;
+		}
+
+		
+		public void SaveEditCustomer(NameValueCollection form)
+		{
+			try
+			{
+				using (SqlConnection con = new SqlConnection(connectionString))
+				{
+					string query = @"UPDATE CustomerMaster SET customer_name = '" + form["CustomerName"] + "', mobile_no = '" + form["Mobileno"] + "',gender = '" + form["Gender"] + "', email_id = '" + form["EmailID"] + "', address = '" + form["Address"] + "', pincode = '" + form["Pincode"] + "' WHERE id = "+ form["UpdateRecordId"] + "";
+
+					SqlCommand cmd = new SqlCommand(query, con);
+					con.Open();
+					cmd.ExecuteNonQuery();
+					con.Close();
+				}
+
+			}
+			catch (Exception ex)
+			{
+				
+			}
+		}
+
+		
+		public DataSet CheckDuplicateRecord(string ColName, string Value)
+		{
+			DataSet ds = new DataSet();
+
+			try
+			{
+				using (SqlConnection conn = new SqlConnection(connectionString))
+				{
+					string query = "SELECT COUNT("+ ColName + ") as RESULT FROM CustomerMaster where "+ ColName + " = '"+ Value + "'";
+					using (SqlDataAdapter adapter = new SqlDataAdapter(query, conn))
+					{
+						adapter.Fill(ds);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error fetching customer data: " + ex.Message);
+			}
+
+			return ds;
+		}
+
+	
+		public int DeleteCustomerRecord(string ID)
+		{
+			try
+			{
+				using (SqlConnection con = new SqlConnection(connectionString))
+				{
+					string query = @"delete from CustomerMaster WHERE id = " + ID + "";
+
+					SqlCommand cmd = new SqlCommand(query, con);
+					con.Open();
+					cmd.ExecuteNonQuery();
+					con.Close();
+				}
+
+			}
+			catch (Exception ex)
+			{
+				
+			}
+			return 0;
+		}
+
+		public DataSet GetCustomerData()
+		{
+			DataSet ds = new DataSet();
+
+			try
+			{
+				using (SqlConnection conn = new SqlConnection(connectionString))
+				{
+					string query = "SELECT * FROM CustomerMaster";
+					using (SqlDataAdapter adapter = new SqlDataAdapter(query, conn))
+					{
+						adapter.Fill(ds);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error fetching customer data: " + ex.Message);
+			}
+
+			return ds;
+		}
+	}
+}
---
+++ CRUD_Assignment_ZesTec/Controllers/CRUDController.cs
@@ -0,0 +1,72 @@
+﻿using CRUD_Assignment_ZesTec.BAL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CRUD_Assignment_ZesTec.Controllers
+{
+    public class CRUDController : Controller
+    {
+		CustomerBAL ObjCust = new CustomerBAL();
+        public ActionResult Index()
+        {
+			ViewBag.GetCustomerData = ObjCust.GetCustomerData();
+			return View();
+        }
+
+		
+		public ActionResult SaveAddCustomer()
+		{
+			NameValueCollection Form = Request.Form;
+			ObjCust.SaveAddCustomer(Form);
+			TempData["Message"] = "Customer Record Added Successfully...!!";
+			return RedirectToAction("Index", "CRUD");
+		}
+		
+		public ActionResult EditCustomer(int ID)
+		{
+			var jsondata = "";
+			Dictionary<string, object> Param = new Dictionary<string, object>();
+
+			Param.Add("CustomerData", ObjCust.GetEditCustomerData(ID).Tables[0]);
+			jsondata = JsonConvert.SerializeObject(Param);
+			return Json(jsondata);
+		}
+		
+		public ActionResult SaveEditCustomer()
+		{
+			NameValueCollection Form = Request.Form;
+			ObjCust.SaveEditCustomer(Form);
+			TempData["Message"] = "Customer Record Updated Successfully...!!";
+			return RedirectToAction("Index", "CRUD");
+		}
+
+		
+		public ActionResult DeleteCustomerRecord(string ID)
+		{
+			ObjCust.DeleteCustomerRecord(ID);
+			TempData["Message"] = "Customer Record Delete Successfully...!!";
+			return RedirectToAction("Index", "CRUD");
+		}
+
+		
+		[HttpPost]
+		public JsonResult CheckDuplicateRecord(string ColName, string Value)
+		{
+			string result = "0";
+			DataSet DS = ObjCust.CheckDuplicateRecord(ColName, Value);
+
+			if (DS.Tables.Count > 0 && DS.Tables[0].Rows.Count > 0)
+			{
+				result = DS.Tables[0].Rows[0]["RESULT"].ToString();
+			}
+			return Json(result);
+		}
+	}
+}
